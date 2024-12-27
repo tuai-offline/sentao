489993d58125085227db875fe3c3899958dd9a79
Edgar Araújo <edgararaj@gmail.com>
2024-12-27 16:15:51 +0000

    Adjusting lexer to fulfill examples

--- tp2.py
+++ tp2.py
@@ -1,80 +1,145 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i python3 -p "python3.withPackages(ps: [ ps.ply ])"
 import ply.lex as lex
-import ply.yacc as yacc
+import sys
 
-
-literals = ['{', '}', '[', ']', '(', ')']
+literals = '{}[]()=,'
 reserved = {
-    'se' : 'SE',
-    'entao' : 'ENTAO',
-    'senao' : 'SENAO',
-    'para' : 'PARA',
-    'faca' : 'FACA',
-    'enquanto' : 'ENQUANTO',
-    'retorna' : 'RETORNA',
-    'leia' : 'LEIA',
-    'escreva' : 'ESCREVA', # imprime?
-    'erro' : 'ERRO',
-    'intervalo' : 'INTERVALO',
-    'repita' : 'REPITA',
-    'ate' : 'ATE',
-    'que' : 'QUE',
-    'ig' : 'IG'
+    'ate': 'ATE',
+    'auto': 'AUTO',
+    'enquanto': 'ENQUANTO',
+    'entao': 'ENTAO',
+    'erro': 'ERRO',
+    'escreva': 'ESCREVA',
+    'faca': 'FACA',
+    'intervalo': 'INTERVALO',
+    'leia': 'LEIA',
+    'no': 'NO',
+    'para': 'PARA',
+    'que': 'QUE',
+    'repita': 'REPITA',
+    'retorna': 'RETORNA',
+    'se': 'SE',
+    'senao': 'SENAO'
+}
 
-    }
-tokens =  ['ID', 'STRING', 'INT', 'REAL', 'MAIS', 'MENOS', 'MULT', 'DIV'] + list(reserved.values())
+tokens = [
+    'ID', 'STRING', 'INT', 'REAL', 'SOMA', 'SUBT', 'MULT', 'DIV', 'MAIORIG',
+    'MENORIG', 'MAIORQ', 'MENORQ', 'IG', 'DIF'
+] + list(reserved.values())
+
 
 def t_STRING(t):
-  r'\"[^\"]*\"'
-  return t
+    r'\"[^\"]*\"'
+    return t
+
 
 def t_ID(t):
-  r'[a-zA-Z]\w{0,19}'
-  t.type = reserved.get(t.value,'ID')  
-  return t
+    r'[a-zA-Z]\w{0,19}'
+    t.type = reserved.get(t.value, 'ID')
+    return t
+
 
 def t_REAL(t):
-  r'\d+\.\d+'
-  return t
+    r'\d+\.\d+'
+    return t
+
 
 def t_INT(t):
-  r'\d+'
-  return t
+    r'\d+'
+    return t
 
-def t_MAIS(t):
-  r'\+'
-  return t
 
-def t_MENOS(t):
-  r'-'
-  return t
+def t_SOMA(t):
+    r'\+'
+    return t
+
+
+def t_SUBT(t):
+    r'-'
+    return t
+
 
 def t_MULT(t):
-  r'\*'
-  return t
+    r'\*'
+    return t
+
 
 def t_DIV(t):
-  r'/'
-  return t
+    r'/'
+    return t
+
+
+def t_MAIORIG(t):
+    r'>='
+    return t
+
+
+def t_MENORIG(t):
+    r'<='
+    return t
+
+
+def t_MAIORQ(t):
+    r'>'
+    return t
+
+
+def t_MENORQ(t):
+    r'<'
+    return t
+
+
+def t_IG(t):
+    r'=='
+    return t
+
+
+def t_DIF(t):
+    r'!='
+    return t
+
 
 def t_newline(t):
-  r'\n+'
-  t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
 
 t_ignore = ' \t'
 
+
 def t_error(t):
-  print(f'Carácter inválido: {t.value[0]}, na linha {t.lexer.lineno}')
+    print(f'Carácter inválido: {t.value[0]}, na linha {t.lexer.lineno}')
+    t.lexer.skip(1)
 
-
-  t.lexer.skip(1)
 
 lexer = lex.lex()
+if __name__ == '__main__':
+    if (len(sys.argv) > 1):
+        f = open(sys.argv[1])
+        lexer.input(f.read())
+        f.close()
+        while tok := lexer.token():
+            print(tok.type, end=" ")
+        print()
+        exit()
 
-data = '''se (10 * 3 ig 30) entao { 
-escreva (10)}'''
+    while True:
+        lines = []
+        print("Digite linhas de texto (pressione Ctrl+D para finalizar):")
 
-lexer.input(data)
+        try:
+            while True:
+                line = input()
+                lines.append(line)
+        except EOFError:
+            pass
 
-for tok in lexer:
-  print(tok)
+        if not lines:
+            break
 
+        source = '\n'.join(lines)
+        lexer.input(source)
+        while tok := lexer.token():
+            print(tok.type, end=" ")
+        print()
