fd45e90f8f761b0f1dae0fbd5bdef79d4b1e8e11
gabrielcp450 <gspaiva2@gmail.com>
2024-12-27 15:52:08 +0000

    lexer nao completo

---
+++ tp2.py
@@ -0,0 +1,79 @@
+import ply.lex as lex
+import ply.yacc as yacc
+
+
+literals = ['{', '}', '[', ']', '(', ')']
+reserved = {
+    'se' : 'SE',
+    'entao' : 'ENTAO',
+    'senao' : 'SENAO',
+    'para' : 'PARA',
+    'faca' : 'FACA',
+    'enquanto' : 'ENQUANTO',
+    'retorna' : 'RETORNA',
+    'leia' : 'LEIA',
+    'escreva' : 'ESCREVA', # imprime?
+    'erro' : 'ERRO',
+    'intervalo' : 'INTERVALO',
+    'repita' : 'REPITA',
+    'ate' : 'ATE',
+    'que' : 'QUE',
+    'ig' : 'IG'
+
+    }
+tokens =  ['ID', 'STRING', 'INT', 'REAL', 'MAIS', 'MENOS', 'MULT', 'DIV'] + list(reserved.values())
+
+def t_STRING(t):
+  r'\"[^\"]*\"'
+  return t
+
+def t_ID(t):
+  r'[a-zA-Z]\w{0,19}'
+  t.type = reserved.get(t.value,'ID')  
+  return t
+
+def t_REAL(t):
+  r'\d+\.\d+'
+  return t
+
+def t_INT(t):
+  r'\d+'
+  return t
+
+def t_MAIS(t):
+  r'\+'
+  return t
+
+def t_MENOS(t):
+  r'-'
+  return t
+
+def t_MULT(t):
+  r'\*'
+  return t
+
+def t_DIV(t):
+  r'/'
+  return t
+
+def t_newline(t):
+  r'\n+'
+  t.lexer.lineno += len(t.value)
+
+t_ignore = ' \t'
+
+def t_error(t):
+  print(f'Carácter inválido: {t.value[0]}, na linha {t.lexer.lineno}')
+
+
+  t.lexer.skip(1)
+
+lexer = lex.lex()
+
+data = '''se (10 * 3 ig 30) entao { 
+escreva (10)}'''
+
+lexer.input(data)
+
+for tok in lexer:
+  print(tok)
