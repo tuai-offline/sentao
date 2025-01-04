a253b9321c17f7572c7c837b9b99aa4cf654cbbe
Edgar Araújo <edgararaj@gmail.com>
2025-01-04 12:20:47 +0000

    Melhoria na cli, e ficheiro de saida

--- parser.py
+++ parser.py
@@ -1,6 +1,9 @@
 from lexer import tokens
 import ply.yacc as yacc
 import sys
+import os
+import argparse
+import io
 
 precedence = (
     ('left', 'E', 'OU'),  # Operadores lógicos
@@ -10,6 +13,14 @@ precedence = (
     ('left', 'MULT', 'DIV', 'MOD'),  # Multiplicação, divisão e módulo
     ('right', 'SUBTU', 'NEG'),  # Operadores unários (negativo)
 )
+
+
+def writevm(instruction):
+    output_file.write(f"{instruction}\n")
+
+
+def print_err(s):
+    print(f"\033[91m{s}\033[0m")
 
 
 def p_programa_acao(p):
@@ -33,7 +44,7 @@ def p_acao_DeclVariavel(p):
 def p_acao_DeclFuncao(p):
     '''Acao : DeclFuncao'''
     p[0] = p[1]
-    print('return')
+    writevm('return')
 
 
 def p_acao_Atribuicao(p):
@@ -66,7 +77,7 @@ def p_fim_escopo(p):
     '''fim_escopo :'''
     removed_scope = parser.decls.pop()
     last_sp = parser.scope_stack.pop()
-    print(f'pop {parser.sp - last_sp}')
+    writevm(f'pop {parser.sp - last_sp}')
     parser.sp = last_sp
 
 
@@ -131,10 +142,10 @@ def p_atribuicao(p):
 
     for scope in reversed(parser.decls):
         if p[1] in scope:
-            print(f"storeg {scope[p[1]][0]}")
+            writevm(f"storeg {scope[p[1]][0]}")
             return
 
-    print(f"Variável {p[1]} não declarada em nenhum escopo!")
+    print_err(f"Variável '{p[1]}' não declarada em nenhum escopo!")
     parser.exito = False
     return
 
@@ -146,11 +157,11 @@ def p_decl_atri_variavel_id(p):
 
     for scope in reversed(parser.decls):
         if p[1] in scope:
-            print("Variavel já declarada!")
+            print_err(f"Variavel '{p[1]}' já declarada!")
             parser.exito = False
             return
 
-    print("pushi 0")
+    writevm("pushi 0")
     parser.decls[-1][p[1]] = parser.sp
     parser.sp += 1
 
@@ -158,10 +169,11 @@ def p_decl_atri_variavel_id(p):
 def p_decl_atri_variavel(p):
     '''DeclAtriVariavel : ID "=" Expressao'''
 
-    if p[1] in parser.decls:
-        print("Variavel já declarada!")
-        parser.exito = False
-        return
+    for scope in reversed(parser.decls):
+        if p[1] in parser.decls:
+            print_err(f"Variavel '{p[1]}' já declarada!")
+            parser.exito = False
+            return
 
     parser.decls[-1][p[1]] = (parser.sp, p[3][1])
     parser.sp += p[3][1]
@@ -170,8 +182,8 @@ def p_decl_atri_variavel(p):
 
 def p_funcao(p):
     '''Funcao : ID "(" ")"'''
-    print(f'pusha {p[1]}')
-    print('call')
+    writevm(f'pusha {p[1]}')
+    writevm('call')
 
 
 def p_indexacao(p):
@@ -181,16 +193,16 @@ def p_indexacao(p):
         if p[1] in scope:
             p[0] = ('INT', 1)
             if int(p[3]) < -scope[p[1]][1] or int(p[3]) >= scope[p[1]][1]:
-                print(f"Acesso em array '{p[1]}' fora dos limites!")
+                print_err(f"Acesso em array '{p[1]}' fora dos limites!")
                 break
             if (int(p[3]) >= 0):
                 idx = scope[p[1]][0] + int(p[3])
             else:
                 idx = scope[p[1]][0] + scope[p[1]][1] + int(p[3])
-            print(f"pushg {idx}")
+            writevm(f"pushg {idx}")
             return
     else:
-        print(f"Variável '{p[1]}' não declarada em nenhum escopo!")
+        print_err(f"Variável '{p[1]}' não declarada em nenhum escopo!")
 
     parser.exito = False
     return
@@ -198,61 +210,61 @@ def p_indexacao(p):
 
 def p_funcao_escreve(p):
     '''Funcao : _ESCREVE "(" ARG ")"'''
-    print("writes")
+    writevm("writes")
 
 
 def p_funcao_escrevei(p):
     '''Funcao : _ESCREVEI "(" ARG ")"'''
-    print("writei")
+    writevm("writei")
 
 
 def p_funcao_escrever(p):
     '''Funcao : _ESCREVER "(" ARG ")"'''
-    print("writer")
+    writevm("writer")
 
 
 def p_funcao_ler(p):
     '''Funcao : _LER "(" ")"'''
-    print("read")
+    writevm("read")
 
 
 def p_funcao_leri(p):
     '''Funcao : _LERI "(" ")"'''
-    print('read')
-    print('atoi')
+    writevm('read')
+    writevm('atoi')
 
 
 def p_funcao_lerr(p):
     '''Funcao : _LERR  "(" ")"'''
-    print('read')
-    print('ator')
+    writevm('read')
+    writevm('ator')
 
 
 def p_funcao_ARG(p):
     '''ARG : ID'''
     for scope in reversed(parser.decls):
         if p[1] in scope:
-            print(f"pushg {scope[p[1]][0]}")
+            writevm(f"pushg {scope[p[1]][0]}")
             return
 
-    print(f"Variável {p[1]} não declarada em nenhum escopo!")
+    print_err(f"Variável '{p[1]}' não declarada em nenhum escopo!")
     parser.exito = False
     return
 
 
 def p_funcao_ARG_INT(p):
     '''ARG : INT'''
-    print("pushi", p[1])
+    writevm("pushi", p[1])
 
 
 def p_funcao_ARG_REAL(p):
     '''ARG : REAL'''
-    print("pushf", p[1])
+    writevm("pushf", p[1])
 
 
 def p_funcao_ARG_CARS(p):
     '''ARG : CARS'''
-    print("pushs", p[1])
+    writevm("pushs", p[1])
 
 
 def p_expressao_id(p):
@@ -261,10 +273,10 @@ def p_expressao_id(p):
     for scope in reversed(parser.decls):
         if p[1] in scope:
             p[0] = ('id', p[1])
-            print(f"pushg {scope[p[1]][0]}")
+            writevm(f"pushg {scope[p[1]][0]}")
             return
 
-    print(f"Variável {p[1]} não declarada em nenhum escopo!")
+    print_err(f"Variável '{p[1]}' não declarada em nenhum escopo!")
     parser.exito = False
     return
 
@@ -288,7 +300,7 @@ def p_expressao_bin_soma(p):
     '''Expressao : Expressao SOMA Expressao'''
 
     p[0] = (p[2], p[1], p[3])
-    print('add')
+    writevm('add')
     pass
 
 
@@ -296,7 +308,7 @@ def p_expressao_bin_subt(p):
     '''Expressao : Expressao SUBT Expressao'''
 
     p[0] = (p[2], p[1], p[3])
-    print('sub')
+    writevm('sub')
     pass
 
 
@@ -304,7 +316,7 @@ def p_expressao_bin_mult(p):
     '''Expressao : Expressao MULT Expressao'''
 
     p[0] = (p[2], p[1], p[3])
-    print('mul')
+    writevm('mul')
     pass
 
 
@@ -312,7 +324,7 @@ def p_expressao_bin_div(p):
     '''Expressao : Expressao DIV Expressao'''
 
     p[0] = (p[2], p[1], p[3])
-    print('div')
+    writevm('div')
     pass
 
 
@@ -320,7 +332,7 @@ def p_expressao_bin_mod(p):
     '''Expressao : Expressao MOD Expressao'''
 
     p[0] = (p[2], p[1], p[3])
-    print('mod')
+    writevm('mod')
     pass
 
 
@@ -328,7 +340,7 @@ def p_expressao_bin_menorq(p):
     '''Expressao : Expressao MENORQ Expressao'''
 
     p[0] = (p[2], p[1], p[3])
-    print('inf')
+    writevm('inf')
     pass
 
 
@@ -336,7 +348,7 @@ def p_expressao_bin_maiorq(p):
     '''Expressao : Expressao MAIORQ Expressao'''
 
     p[0] = (p[2], p[1], p[3])
-    print('sup')
+    writevm('sup')
     pass
 
 
@@ -344,7 +356,7 @@ def p_expressao_bin_menorig(p):
     '''Expressao : Expressao MENORIG Expressao'''
 
     p[0] = (p[2], p[1], p[3])
-    print('infeq')
+    writevm('infeq')
     pass
 
 
@@ -352,7 +364,7 @@ def p_expressao_bin_maiorig(p):
     '''Expressao : Expressao MAIORIG Expressao'''
 
     p[0] = (p[2], p[1], p[3])
-    print('supeq')
+    writevm('supeq')
     pass
 
 
@@ -360,7 +372,7 @@ def p_expressao_bin_ig(p):
     '''Expressao : Expressao IG Expressao'''
 
     p[0] = (p[2], p[1], p[3])
-    print('equal')
+    writevm('equal')
     pass
 
 
@@ -368,7 +380,7 @@ def p_expressao_bin_dif(p):
     '''Expressao : Expressao DIF Expressao'''
 
     p[0] = (p[2], p[1], p[3])
-    print('dif')
+    writevm('dif')
     pass
 
 
@@ -376,7 +388,7 @@ def p_expressao_bin_e(p):
     '''Expressao : Expressao E Expressao'''
 
     p[0] = (p[2], p[1], p[3])
-    print('and')
+    writevm('and')
     pass
 
 
@@ -384,15 +396,15 @@ def p_expressao_bin_ou(p):
     '''Expressao : Expressao OU Expressao'''
 
     p[0] = (p[2], p[1], p[3])
-    print('or')
+    writevm('or')
     pass
 
 
 def p_expressao_un_subt(p):
     '''Expressao : SUBT Expressao %prec SUBTU'''
     p[0] = ('neg', p[2])
-    print('pushi -1')
-    print('mul')
+    writevm('pushi -1')
+    writevm('mul')
 
 
 def p_expressao_un_neg(p):
@@ -408,13 +420,13 @@ def p_expressao_grupo(p):
 def p_tipo_constante_INT(p):
     '''TipoConstante : INT'''
     p[0] = ('INT', 1)
-    print(f"pushi {p[1]}")
+    writevm(f"pushi {p[1]}")
 
 
 def p_tipo_REAL(p):
     '''TipoConstante : REAL'''
     p[0] = ('REAL', 1)
-    print(f"pushf {p[1]}")
+    writevm(f"pushf {p[1]}")
 
 
 def p_tipo_CAR(p):
@@ -444,17 +456,17 @@ def p_l_tipo_constante_lista(p):
 
 def p_condicao_se(p):
     '''Condicao : Se "(" Expressao ")" Entao Escopo'''
-    print(f"{parser.label_stack.pop()}:")
+    writevm(f"{parser.label_stack.pop()}:")
 
 
 def p_condicao_se_senao(p):
     '''Condicao : Se "(" Expressao ")" Entao Escopo Senao Escopo'''
-    print(f"{parser.label_stack.pop()}:")
+    writevm(f"{parser.label_stack.pop()}:")
 
 
 def p_condicao_se_senao_se(p):
     '''Condicao : Se "(" Expressao ")" Entao Escopo Senao Condicao'''
-    print(f"{parser.label_stack.pop()}:")
+    writevm(f"{parser.label_stack.pop()}:")
 
 
 def p_se(p):
@@ -465,16 +477,16 @@ def p_se(p):
 
 def p_entao(p):
     '''Entao : _ENTAO'''
-    print(f"jz {parser.label_stack[-1]}")
+    writevm(f"jz {parser.label_stack[-1]}")
 
 
 def p_senao(p):
     '''Senao : _SENAO'''
     a = parser.label_stack.pop()
-    print(f"jump {parser.label}")
+    writevm(f"jump {parser.label}")
     parser.label_stack.append(parser.label)
     parser.label += 1
-    print(f"{a}:")
+    writevm(f"{a}:")
 
 
 def p_ciclo_para(p):
@@ -486,34 +498,32 @@ def p_ciclo_enquanto(p):
     '''Ciclo : Enquanto "(" Expressao ")" Faca Escopo'''
     a = parser.label_stack.pop()
     b = parser.label_stack.pop()
-    print(f'jump {b}')
-    print(f'{a}:')
+    writevm(f'jump {b}')
+    writevm(f'{a}:')
 
 
 def p_ciclo_enquantoTransicao(p):
     '''Enquanto : _ENQUANTO'''
-    print(f"{parser.label}:")
+    writevm(f"{parser.label}:")
     parser.label_stack.append(parser.label)
     parser.label += 1
 
 
 def p_ciclo_enquanto_faca(p):
     '''Faca : _FACA'''
-    print(f'jz {parser.label}')
+    writevm(f'jz {parser.label}')
     parser.label_stack.append(parser.label)
     parser.label += 1
 
 
 def p_ciclo_repita(p):
     '''Ciclo : Repita Escopo _ATE _QUE "(" Expressao ")"'''
-
-    print(f"jz {parser.label_stack.pop()}")
+    writevm(f"jz {parser.label_stack.pop()}")
 
 
 def p_ciclo_repitaTRANS(p):
     '''Repita : _REPITA'''
-
-    print(f"{parser.label}:")
+    writevm(f"{parser.label}:")
     parser.label_stack.append(parser.label)
     parser.label += 1
 
@@ -526,8 +536,8 @@ def p_decl_funcao(p):
 def p_decl_id_funcao(p):
     '''IdFuncao : ID'''
     parser.fun_decls.append(p[1])
-    print(f'{p[1]}:')
-    print(f'start')
+    writevm(f'{p[1]}:')
+    writevm(f'start')
 
 
 def p_error(p):
@@ -545,29 +555,50 @@ def init_parser():
 
 
 def preludio():
-    print("jump inicio")
+    writevm("jump inicio")
 
 
 def epilogo():
     if 'inicio' not in parser.fun_decls:
-        print("Função 'inicio' não declarada!")
+        print_err("Função 'inicio' não declarada!")
 
 
 if __name__ == '__main__':
     parser = yacc.yacc(debug=True, write_tables=True)
     init_parser()
 
-    if (len(sys.argv) > 1):
-        f = open(sys.argv[1])
-        preludio()
-        parser.parse(f.read(), tracking=True)
-        epilogo()
-        f.close()
-        if parser.exito:
-            print("\033[92mAnálise sintática concluída com sucesso!\033[0m")
+    parser_args = argparse.ArgumentParser()
+    parser_args.add_argument('input_file',
+                             nargs='?',
+                             help='input file to parse',
+                             default=None)
+    parser_args.add_argument('-o',
+                             '--output',
+                             help='output file name',
+                             default='output.ewvm')
+    args = parser_args.parse_args()
+
+    if args.input_file:
+        output_file = io.StringIO()
+        with open(args.input_file, 'r') as f:
+            preludio()
+            parser.parse(f.read(), tracking=True)
+            epilogo()
+            if parser.exito:
+                print(
+                    "\033[92mAnálise sintática concluída com sucesso!\033[0m")
+
+                if os.path.exists(args.output):
+                    print(
+                        f"Ficheiro '{args.output}' já existente. Sobrescrevendo!"
+                    )
+
+                with open(args.output, 'w') as out:
+                    out.write(output_file.getvalue())
         exit()
 
     while True:
+        output_file = sys.stdout
         init_parser()
         lines = []
         print("Digite linhas de texto (pressione Ctrl+D para finalizar):")
@@ -583,6 +614,7 @@ if __name__ == '__main__':
             break
 
         source = '\n'.join(lines)
+        print("==== Instruções EWVM ====")
         preludio()
         parser.parse(source, tracking=True)
         epilogo()
